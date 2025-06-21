cb9cff67bb52ad5c9e8e17b2787f2a910dec9d19
colorxad <62230947+glenpi4376@users.noreply.github.com>
2025-06-21 18:09:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,19 +21,19 @@ app.listen(1111, () => {
     console.log("Server is Live");
 });
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb://127.0.0.1:27017";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb://127.0.0.1:27017";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-const database = client.db("add-to-production");
-const colls = database.listCollections();
+// const database = client.db("add-to-production");
+//const colls = database.listCollections();
 
 // async function run() {
 //   try {
